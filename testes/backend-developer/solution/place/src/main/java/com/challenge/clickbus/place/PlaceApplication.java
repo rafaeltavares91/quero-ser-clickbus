f9d1b6aa827b1ec59f7e910e8cfb481a4c4d12b0
Rafael Tavares <rafaeltavares91@gmail.com>
2019-12-20 21:36:44 -0300

    First commit sharing spring initializr generated project.

---
+++ testes/backend-developer/solution/place/src/main/java/com/challenge/clickbus/place/PlaceApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.clickbus.place;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceApplication.class, args);
+	}
+
+}
